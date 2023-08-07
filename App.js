7b7af1ae948b2375486e903d0ca4ba4318845476
Ansonsd5 <anson11sd@gmail.com>
2023-08-07 23:56:39 +0530

    import react and react dom

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
       React.createElement("h1", {}, "I'm an h1 tag"),
@@ -9,8 +12,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     ]),
   ]);
   
-  
-  console.log(parent); 
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   
