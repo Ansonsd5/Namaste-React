491d98d1a653fa53e452b6f04adb289336cd06e1
Ansonsd5 <anson11sd@gmail.com>
2023-08-09 23:32:38 +0530

    props for restaurant

--- Body.js
+++ Body.js
@@ -1,16 +1,24 @@
 import React from "react";
-import food  from "./assets/images/foodImg.png"
+import food from "./assets/images/foodImg.png";
+
+const RestaurantCard = (props) => (
+  <div className="restaurantCard">
+    <img src={food} />
+    <div className="restaurantName">{props.restaurantName}</div>
+    <div className="dishName">{props.cuisine}</div>
+    <div className="ratings">{props.ratings}</div>
+  </div>
+);
 
 export const Body = () => (
   <div className="bodyWrapper">
     <div className="search">Search</div>
     <div className="restaurantWrapper">
-      <div className="restaurantCard">
-        <img src={food} />
-        <div className="dishName">Briyani</div>
-        <div>Other Details</div>
-      </div>
-
+      <RestaurantCard restaurantName="Tikka Hut" ratings="4.0" cuisine="Tikka"/>
+      <RestaurantCard restaurantName="Hot Pot" ratings="3.9" cuisine="Briyani"/>
+      <RestaurantCard restaurantName="Royal Food" ratings="4.5" cuisine="Noodels"/>
+      <RestaurantCard restaurantName="Chai point" ratings="4.2" cuisine="Tea"/>
+      
     </div>
   </div>
 );
