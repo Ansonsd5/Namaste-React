3f25ac7388d51ebc78a1c2e574744687595f8ddc
Ansonsd5 <anson11sd@gmail.com>
2023-08-14 23:48:44 +0530

    css chnages

--- src/Body.js
+++ src/Body.js
@@ -15,36 +15,35 @@ export const Body = () => {
 
   const fetchData = async () => {
       const response = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.000761&lng=74.8185446&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://www.swiggy.com/api/seo/getListing?lat=12.960059122809971&lng=77.57337538383284"
       );
       const jsonData = await response.json();
-      const requiredData =
-        jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
-          ?.restaurants;
+      // const requiredData =
+      //   jsonData?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle
+      //     ?.restaurants;
+          const requiredData = jsonData?.data?.success?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
       setListOfRestaurant(requiredData);
       setFilteredRestaurant(requiredData);
     }
 
-
   const handleSearchText = () => {
+    
     const filteredRestaurant = listOfRestaurant.filter((res) =>
       res.info.name.toLowerCase().includes(searchText.toLowerCase())
     );
     setFilteredRestaurant(filteredRestaurant);
   };
 
-  
-
-  return  listOfRestaurant.length ===0  ? <Shimmer />: (
+ 
+  return  filteredRestaurant.length === 0  ? <Shimmer />: (
     <div className="bodyWrapper">
-    
       <div className="searchBoxWrapper">
         <div className="search">
           <input
             className="searchBox"
             type="text"
             onChange={(e) => {
-              setSearchText(e.target.value), handleSearchText();
+              setSearchText(e.target.value);
             }}
             value={searchText}
           />
@@ -52,7 +51,7 @@ export const Body = () => {
         </div>
       </div>
       <div className="restaurantWrapper">
-        {filteredRestaurant && filteredRestaurant.map((restaurant) => (
+        { filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} /> 
         )) }
       </div>
