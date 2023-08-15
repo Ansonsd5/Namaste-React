86ac1c1ade63036c26b5d13caf10b3df13b6226c
Ansonsd5 <anson11sd@gmail.com>
2023-08-15 13:29:51 +0530

    feat: login button added

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import heroiconImage from '../assets/images/heroicon.png';
 
 
-export const Header = () => (
-  <div className="headerWrapper">
+export const Header = () => {
+  const [isLogin,setIsLogin] = useState(false);
+  const handleLogin = () =>{
+   if (isLogin) {
+      setIsLogin(false)
+    } else {
+      setIsLogin(true)
+    }
+  }
+return   <div className="headerWrapper">
     <div className="logoWrapper">
       <img src={heroiconImage} />
     </div>
@@ -14,9 +22,10 @@ export const Header = () => (
           <li>About Us</li>
           <li>Cart</li>
           <li>Conatct Us</li>
+          <li><button onClick={handleLogin}>{isLogin? "Logout" :"Login" }</button></li>
         </ul>
       </div>
       
     </div>
   </div>
-);
+};
