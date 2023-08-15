2da20bfd6a3ab813c769a0504233af2651251c6c
Ansonsd5 <anson11sd@gmail.com>
2023-08-15 15:17:16 +0530

    feat: route implementation with children routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import { Header } from "./Header";
 import { Body } from "./Body";
@@ -9,33 +8,35 @@ import AboutUs from "./Components/About";
 import Contactus from "./Components/Contactus";
 import Error from "./Components/Error";
 
-
-const AppLayout = () =>{
-return (
-  <div className="app">
-    <Header />
-    <Body />
-  
-  </div>
-)
-} 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
 const appRouters = createBrowserRouter([
   {
-    path:"/",
-    element:<AppLayout />,
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/contact",
+        element: <Contactus />,
+      },
+    ],
     errorElement: <Error />,
   },
-  {
-    path:"/about",
-    element: <AboutUs />,
-    errorElement: <Error />,
-  },
-  {
-    path:"/contact",
-    element: <Contactus />,
-    errorElement: <Error />,
-  }
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouters} />);
