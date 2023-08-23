fb055dbc60a8484c8897a5395c0613b006b34d76
Ansonsd5 <anson11sd@gmail.com>
2023-08-23 21:12:37 +0530

    feat: implemented about us by using class component

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,23 +3,54 @@ import React from 'react';
 import UserClass from './UserClass';
 import UserFunction from './UserFunction';
 
-const AboutUs = () => {
-  return (
-    <section className="about-us">
-      <div className="about-content">
-        <h1>About Us</h1>
-        <UserFunction name={"Anson Saver Dsouza"} role={"Frontend Developer @Niveus Solution"} workLocation= {"Mangaluru Karnataka"}/>
-       
-        <UserClass name={"Renol"} role={"FrontEnd Developer"} company={"Niveus Solutions"} workLocation= {"Mangalore Karnataka"}/>
-      </div>
-      <div className="about-image">
-        <img src="about-image.jpg" alt="Team at work" />
-      </div>
-      
-    </section>
-    
-  );
-};
+class AboutUsClass extends React.Component{
+constructor(props){
+ super(props)
+  console.log("Parent constructor");
+}
 
-export default AboutUs;
+componentDidMount(){
+  console.log("Parent did Mount");
+}
+  render(){
+    console.log("Parent Render");
+    return (
+      <section className="about-us">
+        <div className="about-content">
+          <h1>About Us</h1>
+          <UserFunction name={"Anson Saver Dsouza"} role={"Frontend Developer @Niveus Solution"} workLocation= {"Mangaluru Karnataka"}/>
+         
+          <UserClass name={"Renol"} role={"FrontEnd Developer"} company={"Niveus Solutions"} workLocation= {"Mangalore Karnataka"}/>
+        </div>
+        <div className="about-image">
+          <img src="about-image.jpg" alt="Team at work" />
+        </div>
+        
+      </section>
+      
+    );
+  }
+
+}
+export default AboutUsClass;
+
+// const AboutUs = () => {
+//   return (
+//     <section className="about-us">
+//       <div className="about-content">
+//         <h1>About Us</h1>
+//         <UserFunction name={"Anson Saver Dsouza"} role={"Frontend Developer @Niveus Solution"} workLocation= {"Mangaluru Karnataka"}/>
+       
+//         <UserClass name={"Renol"} role={"FrontEnd Developer"} company={"Niveus Solutions"} workLocation= {"Mangalore Karnataka"}/>
+//       </div>
+//       <div className="about-image">
+//         <img src="about-image.jpg" alt="Team at work" />
+//       </div>
+      
+//     </section>
+    
+//   );
+// };
+
+// export default AboutUs;
 
--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -2,30 +2,39 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
+    console.log("Child constructor");
     super(props);
 
     this.state = {
        count: 0,
+       unlike:0
       
     }
    
   }
-
+  componentDidMount(){
+    console.log("child component did mount");
+  }
   render() {
+    {console.log("Child Render")}
     const { name, role, company, workLocation } = this.props;
-    const { count } = this.state;
+    const { count , unlike} = this.state;
     const updateCount = () =>{
-      console.log(count);
-      this.setState({count : count + 1})
+      this.setState({count : this.state.count + 1})
     }
+
+   
     return (
+      
       <div className="classComponent" >
         <h1>This is a Class Based React Component</h1>
         <h1 style={{color:"red"}}>{name}</h1>
         <h2>{role + " " + company}</h2>
         <h5>{workLocation}</h5>
         <h4>Count : {count}</h4>
-       <button onClick={updateCount}>Click me to Update Count</button>
+        <h4>unLike : {unlike}</h4>
+       <button onClick={updateCount}>Like</button>
+       <button onClick={() => {this.setState({unlike :this.state.unlike -1})}}>disLike</button>
       </div>
     );
   }
