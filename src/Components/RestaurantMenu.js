66ac7c825b988586466cfa13412133fb8183643b
Ansonsd5 <anson11sd@gmail.com>
2023-08-27 00:05:33 +0530

    feat: implemented custom hook to fetch data

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -2,16 +2,12 @@ import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
- 
-
   const { resId } = useParams();
- 
 
   const resInfo = useRestaurantMenu(resId);
-  
-  
+
   console.log(resInfo, "resInfo");
-  // console.log(menuItem, "menuItem");
+
   return (
     <>
       {resInfo ? (
@@ -21,24 +17,6 @@ const RestaurantMenu = () => {
           <div>{resInfo.avgRating}</div>
           <hr></hr>
           <div>{resInfo.costForTwoMessage}</div>
-          <div className="flatDealWrapper">
-            {/* <div className="menuItemGrid">
-              {menuItem.map((item) => (
-                <div className="menuList" key={item.card.info.id}>
-                  <h5>{item.card.info.name}</h5>
-                  
-                  <img className="menuItemImg"
-                    src={
-                      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/" +
-                      item.card.info.imageId
-                    }
-                  />{" "}
-                  <p>₹{item.card.info.price / 100}</p>
-                  <p className="dishDiscription">{item.card.info.description}</p>
-                </div>
-              ))}
-            </div> */}
-          </div>
         </div>
       ) : (
         <div>Some Things is wrong... we will be back</div>
