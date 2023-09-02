2bf8eabe52a2b98993674404ba5050d0974661fe
Ansonsd5 <anson11sd@gmail.com>
2023-09-02 09:09:15 +0530

    feat: implemented Restaurant Menu

--- src/Components/RestaurantCatagories.js
+++ src/Components/RestaurantCatagories.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+
 const RestaurantCatagories = ({ data }) => {
+  console.log(data);
   return (
     <div>
       {data.map((dish, index) => (
-        <div className="bg-red-300 my-2 " key={dish?.card?.info?.id + index}>{dish?.card?.info?.name}</div>
+        <div className="bg-green-100 my-2 flex p-8" key={dish?.card?.info?.id + index}>
+          <div className="w-2/10 h-9"><img className="w-9/4 h-8" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+dish?.card?.info?.imageId}></img></div>
+          {dish?.card?.info?.name}
+          </div>
       ))}
     </div>
   );
--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,13 +1,16 @@
+import React from 'react';
+
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCatagories from "./RestaurantCatagories";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
+  const [showCatagoryItems, setShowCatagoryItems] = useState(false);
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
   const restaurantName = resInfo?.cards[0]?.card?.card?.info?.name;
-  // console.log(resInfo?.cards[0]?.card?.card?.info?.name, "resInfo");
 
   const requiredDataForMenu =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
@@ -16,27 +19,33 @@ const RestaurantMenu = () => {
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
     );
 
-  console.log(requiredDataForMenu, "requiredDataForMenu");
+  const handleAccordian = () => {
+    setShowCatagoryItems(!showCatagoryItems);
+  };
 
   return (
     <div>
-      {/* Restaurant Name */}
       <div className="font-bold text-xxl text-center">{restaurantName}</div>
       <div className="p-10">
-        {requiredDataForMenu?.map((catagory, index) => (<div>
-          <div className="flex justify-around bg-primaryBg py-4 my-3" key={catagory?.card?.card.title} >
-            <div className="w-9/12">{catagory?.card?.card.title}</div>
-            <div>⮟</div>
+        {requiredDataForMenu?.map((catagory, index) => (
+          <div>
+            <div
+              className="flex justify-around bg-primaryBg py-4 my-3 cursor-pointer"
+              key={catagory?.card?.card.title}
+              onClick={handleAccordian}
+            >
+              <div className="w-16">{catagory?.card?.card.title}</div>
+              <div>⮟</div>
             </div>
-            <div className="flex">
-            <RestaurantCatagories data={catagory?.card?.card?.itemCards}/>
+            <div>
+              {setShowCatagoryItems && (
+                <RestaurantCatagories data={catagory?.card?.card?.itemCards} />
+              )}
+            </div>
+            <div></div>
           </div>
-          <div></div>
-        </div>))}
-        {/* <ItemCategory key={"ddddd"+restaurantName} cataData ={requiredDataForMenu}/> */}
+        ))}
       </div>
-
-      {/* Accordian */}
     </div>
   );
 };
