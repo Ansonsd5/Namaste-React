98a41dd8bd1e0a262e76dca8c982a12da0479846
Ansonsd5 <anson11sd@gmail.com>
2023-09-10 23:30:46 +0530

    feat: implemented cart component and functionality

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -18,9 +18,11 @@ const Cart = () =>{
     return <div className="w-6/12 flex  flex-col m-auto">
        
         <h1 className="  text-xl font-bold text-center">Cart</h1>
-        <button className="w-2/12 bg-primary rounded text-white text-center m-auto p-2 my-2" onClick={clearCartHandler}>Clear cart</button>
+        {console.log(cartItems.length ,"cartItems.length")}
+   {cartItems.length != 0?
+        <button className="w-2/12 bg-primary rounded text-white text-center m-auto p-2 my-2" onClick={clearCartHandler}>Clear cart</button>: <div className="text-xl font-bold text-center bg-red-100 p-2 rounded-xl">Cart is empty!!!!! Lets try new cuisine </div>
         
-        
+   }
         <div>
            <ListItems items={cartItems}/>
         </div>
