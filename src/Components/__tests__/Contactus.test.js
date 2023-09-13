fa21e3a469a93d4ac9f0c407203a60f0371dcd4e
Ansonsd5 <anson11sd@gmail.com>
2023-09-13 23:54:08 +0530

    test:unit  test cases for conatct us

---
+++ src/Components/__tests__/Contactus.test.js
@@ -0,0 +1,24 @@
+import {screen, render, getAllByRole } from "@testing-library/react"
+import Contactus from "../Contactus";
+import "@testing-library/jest-dom";
+
+
+test("Test if the conatct us component has header",()=>{
+
+    render(<Contactus/>);
+
+    const header = screen.getByRole('heading');
+
+expect(header).toBeInTheDocument();
+});
+
+test("Test if the submit button present in the component", () =>{
+    render(<Contactus />);
+
+    const button = screen.getByText('Submit');
+
+    expect(button).toBeInTheDocument();
+
+});
+
+
